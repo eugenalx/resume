7b36e7b55a7eb166d964162e928b285c993102fa
Eugen <eugenalx@gmail.com>
2022-02-05 19:48:32 +0200

    Backup.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function hamburgerMenu(x) {
     x.classList.toggle("change");
   }
+
+//   slider functions
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+// end slider functions
