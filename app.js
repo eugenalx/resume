172bf15ff3f6cfbcf19182d90f9b48b8661d2b55
Eugen <eugenalx@gmail.com>
2022-02-13 09:11:55 +0200

    display dinamyc first page

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
+const Helloo = {
+  myexperience : {
+    nume: "my Experience",
+    image: "images/experience.jpg"
+  },
+
+  mystudies: {
+    nume: "my Studies",
+    image: "images/experience.jpg"
+  },
+  myhobbies: {
+    nume: "my Hobbies",
+    image: "images/experience.jpg"
+  },
+  myskills: {
+    nume: "my Skills",
+    image: "images/experience.jpg"
+  },
+  myprojects: {
+    nume: "my Projects",
+    image: "images/experience.jpg"
+  },
+  myothers: {
+    nume: "my Others",
+    image: "images/experience.jpg"
+  },
+
+  };
+    
+
 function hamburgerMenu(x) {
     x.classList.toggle("change");
   }
+
+function Listare(lista) {
+
+    Object.values(lista).forEach(function(val){
+    // console.log(val.nume);
+    let divMain = document.createElement("div");
+    let paragrafMain = document.createElement("p");
+    let imageMain = document.createElement("img");
+
+    divMain.className="pointer";
+    divMain.onclick="openNav()";
+    paragrafMain.innerHTML = val.nume;
+    imageMain.src = val.image;
+
+    document.getElementById("list").appendChild(divMain).prepend(imageMain,paragrafMain);
+
+  });
+}
 
 //   slider functions
 
